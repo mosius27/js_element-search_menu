58c0d2788b44053dd5fdf4fb8edf1acc5e872e97
mosius27 <mosius27@yandex.ru>
2023-09-23 15:40:55 +0300

    .

--- task.js
+++ task.js
@@ -1,29 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Получаем все элементы с классом "menu__link"
-    const menuLinks = document.querySelectorAll(".menu__link");
-  
-    // Добавляем обработчик события click для каждого элемента
-    menuLinks.forEach(function (link) {
-      link.addEventListener("click", function (event) {
-        // Предотвращаем переход по ссылке
+  // Получаем все элементы с классом "menu__link"
+  const menuLinks = document.querySelectorAll(".menu__link");
+
+  // Добавляем обработчик события click для каждого элемента
+  menuLinks.forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      // Предотвращаем переход по ссылке, если есть вложенное меню
+      if (link.nextElementSibling && link.nextElementSibling.classList.contains("menu_sub")) {
         event.preventDefault();
-  
+
         // Находим ближайший родительский элемент с классом "menu"
         const menu = link.closest(".menu");
-  
+
         // Находим все вложенные меню внутри найденного меню
         const subMenus = menu.querySelectorAll(".menu_sub");
-  
-        // Скрываем все вложенные меню
+
+        // Закрываем все вложенные меню, кроме текущего
         subMenus.forEach(function (subMenu) {
-          subMenu.classList.remove("menu_active");
+          if (subMenu !== link.nextElementSibling) {
+            subMenu.classList.remove("menu_active");
+          }
         });
-  
-        // Если текущий элемент содержит вложенное меню, то открываем его
-        const subMenu = link.nextElementSibling;
-        if (subMenu && subMenu.classList.contains("menu_sub")) {
-          subMenu.classList.toggle("menu_active");
-        }
-      });
+
+        // Открываем или закрываем текущее вложенное меню
+        link.nextElementSibling.classList.toggle("menu_active");
+      }
     });
   });
+});
